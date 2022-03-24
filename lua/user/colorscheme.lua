f18a5e35b7d01992854a2dbaa51efdc84a8f68eb
carlosahs <tacahs12@gmail.com>
2022-03-24 09:03:30 -0600

    Add colorscheme configuration

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,18 @@
+require("github-theme").setup({
+  theme_style = "dark",
+  function_style = "italic",
+  sidebars = { "qf", "vista_kind", "terminal", "packer" },
+
+  -- Change the "hint" and "error" colors
+  colors = { hint = "orange", error = "#ff0000" },
+
+  -- Overwrite the highlight groups
+  overrides = function(c)
+    return {
+      htmlTag = { fg = c.red, bg = "#282c34", sp = c.hint, style = "underline" },
+      DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
+      -- Remove highlight groups
+      TSField = {},
+    }
+  end
+})
