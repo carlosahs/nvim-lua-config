e18fa025028ad2671a8a71dae7bb03ad0305e2b2
carlosahs <tacahs12@gmail.com>
2022-04-04 11:20:44 -0500

    Comment out disable option

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -6,7 +6,7 @@ configs.setup {
   ignore_install = { "" },
   highlight = {
     enable = true,
-    disable = { "" },
+    -- disable = { "" },
     additional_vim_regex_highlighting = false,
   },
 }
