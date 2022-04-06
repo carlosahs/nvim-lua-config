cf456e060bce2e709e23d595794acac7f01f4b9a
carlosahs <tacahs12@gmail.com>
2022-04-06 17:38:21 -0500

    Uncomment sidebars

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,7 @@
 require("github-theme").setup({
   theme_style = "dark",
   function_style = "italic",
-  -- sidebars = { "qf", "vista_kind", "terminal", "packer" },
+  sidebars = { "qf", "vista_kind", "terminal", "packer" },
 
   -- -- Change the "hint" and "error" colors
   -- colors = { hint = "orange", error = "#ff0000" },
