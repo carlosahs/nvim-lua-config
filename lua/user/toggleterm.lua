6734b8cb5668cb73a9ae6167e83c3adced2f324f
carlosahs <tacahs12@gmail.com>
2022-04-06 18:00:23 -0500

    Config float terminal

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -12,7 +12,10 @@ toggleterm.setup({
   shade_terminals = true,
   shading_factor = 2,
   start_in_insert = true,
+  insert_mappings = true,
   persist_size = true,
+  direction = "float",
+  close_on_exit = true,
   shell = vim.o.shell,
   float_opts = {
     border = "curved",
