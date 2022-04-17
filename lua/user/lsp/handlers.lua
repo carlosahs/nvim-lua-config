7797cbbb702be3fd2147ea3465715733ff4f0c80
carlosahs <tacahs12@gmail.com>
2022-04-16 19:11:55 -0500

    Add keymap to rename

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -103,8 +103,8 @@ local function lsp_keymaps(bufnr)
     '<cmd>lua vim.lsp.diagnostic.goto_next({ border = "rounded" })<CR>', opts
   )
   vim.api.nvim_buf_set_keymap(
-    bufnr, "n", "<leader>q",
-    "<cmd>lua vim.lsp.diagnostic.setloclist()<CR>", opts
+    bufnr, "n", "<leader>rn",
+    "<cmd>lua vim.lsp.buf.rename()<CR>", opts
   )
 
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
