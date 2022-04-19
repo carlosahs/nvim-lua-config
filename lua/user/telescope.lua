d0b72f92d4a764ed554a11ea5d92ce41ba8f4628
carlosahs <52082645+carlosahs@users.noreply.github.com>
2022-04-19 08:47:44 -0500

    Uncomment keymaps for insert mode

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -4,61 +4,61 @@ if not status_ok then
   return
 end
 
--- local actions = require "telescope.actions"
+local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
-    -- mappings = {
-    --   i = {
-    --     ["<C-n>"] = actions.cycle_history_next,
-    --     ["<C-p>"] = actions.cycle_history_prev,
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
 
-    --     ["<C-j>"] = actions.move_selection_next,
-    --     ["<C-k>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
 
-    --     ["<C-c>"] = actions.close,
+        ["<C-c>"] = actions.close,
 
-    --     ["<CR>"] = actions.select_default,
-    --     ["<C-x>"] = actions.select_horizontal,
-    --     ["<C-v>"] = actions.select_vertical,
-    --     ["<C-t>"] = actions.select_tab,
+        ["<CR>"] = actions.select_default,
+        ["<C-x>"] = actions.select_horizontal,
+        ["<C-v>"] = actions.select_vertical,
+        ["<C-t>"] = actions.select_tab,
 
-    --     ["<C-u>"] = actions.preview_scrolling_up,
-    --     ["<C-d>"] = actions.preview_scrolling_down,
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
 
-    --     ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-    --     ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
 
-    --     ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-    --     ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-    --     ["<C-l>"] = actions.complete_tag,
-    --     ["<C-_>"] = actions.which_key,
-    --   },
-    --   n = {
-    --     ["<ESC>"] = actions.close,
-    --     ["<CR>"] = actions.select_default,
-    --     ["<C-x>"] = actions.select_horizontal,
-    --     ["<C-v>"] = actions.select_vertical,
-    --     ["<C-t>"] = actions.select_tab,
+        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        ["<C-l>"] = actions.complete_tag,
+        ["<C-_>"] = actions.which_key,
+      },
+      -- n = {
+      --   ["<ESC>"] = actions.close,
+      --   ["<CR>"] = actions.select_default,
+      --   ["<C-x>"] = actions.select_horizontal,
+      --   ["<C-v>"] = actions.select_vertical,
+      --   ["<C-t>"] = actions.select_tab,
 
-    --     ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-    --     ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-    --     ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-    --     ["<M-q>"] = actions.send_selected_to_qflits + actions.open_qflist,
-
-    --     ["j"] = actions.move_selection_next,
-    --     ["k"] = actions.move_selection_previous,
-    --     ["gg"] = actions.move_to_top,
-    --     ["M"] = actions.move_to_middle,
-    --     ["G"] = actions.move_to_bottom,
-
-    --     ["<C-u>"] = actions.preview_scrolling_up,
-    --     ["<C-d>"] = actions.preview_scrolling_down,
-    --   },
-    -- },
+      --   ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+      --   ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+      --   ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+      --   ["<M-q>"] = actions.send_selected_to_qflits + actions.open_qflist,
+    
+      --   ["j"] = actions.move_selection_next,
+      --   ["k"] = actions.move_selection_previous,
+      --   ["gg"] = actions.move_to_top,
+      --   ["M"] = actions.move_to_middle,
+      --   ["G"] = actions.move_to_bottom,
+    
+      --   ["<C-u>"] = actions.preview_scrolling_up,
+      --   ["<C-d>"] = actions.preview_scrolling_down,
+      -- },
+    },
   },
   pickers = {
     find_files = {
