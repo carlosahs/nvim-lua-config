663e8ed443ea503fcbab10292474649cfbc2069c
carlosahs <52082645+carlosahs@users.noreply.github.com>
2022-05-09 16:43:18 -0500

    Ensure parsers for commonly used languages

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup {
-  ensure_installed = "maintained",
+  ensure_installed = { "c", "python", "java", "rust", "lua" },
   sync_install = false,
   ignore_install = { "" },
   highlight = {
